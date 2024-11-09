ca85fe8f9783e7c991ac3570b95723c02c8242e5
rupjyoti921 <rupjyoti921@gmail.com>
2024-11-09 14:18:15 +0530

    learned about abstraction

--- 2_oop/enemy.py
+++ 2_oop/enemy.py
@@ -2,3 +2,12 @@ class Enemy:
     type_of_enemy: str
     health_point: int=10
     attack_damage: int=1
+
+    def talk(self):
+        print(f"I am a {self.type_of_enemy}. Be prepared to fight")
+
+    def walk_forward(self):
+        print(f"{self.type_of_enemy} moves closer to you")
+    
+    def attack(self):
+        print(f"{self.type_of_enemy} attacks for {self.attack_damage} damage")
--- 2_oop/main.py
+++ 2_oop/main.py
@@ -3,3 +3,16 @@ from enemy import *
 ene1=Enemy()
 ene1.type_of_enemy="Zombie"
 print(f"{ene1.type_of_enemy} has {ene1.health_point} health points and can do attack of {ene1.attack_damage}")
+
+ene1.talk()
+ene1.walk_forward()
+ene1.attack()
+print()
+
+ene2=Enemy()
+ene2.type_of_enemy="Danab"
+ene2.attack_damage=5
+ene2.health_point=20
+ene2.talk()
+ene2.walk_forward()
+ene2.attack()
