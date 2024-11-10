d0e12a24a338d34cc516141a9e0c6ae8f9e9875c
rupjyoti921 <rupjyoti921@gmail.com>
2024-11-10 13:12:28 +0530

    learned about encapsulation

---
+++ 2_oop/2.2_encapsulation/enemy.py
@@ -0,0 +1,18 @@
+class Enemy:
+
+    def __init__(self, type_of_enemy, health_points=10,attack_damage=1):    #No Argument Constructor
+         self.__type_of_enemy=type_of_enemy
+         self.health_point=health_points
+         self.attack_damage=attack_damage
+
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
+    
+    def talk(self):
+        print(f"I am a {self.__type_of_enemy}. Be prepared to fight")
+
+    def walk_forward(self):
+        print(f"{self.__type_of_enemy} moves closer to you")
+    
+    def attack(self):
+        print(f"{self.__type_of_enemy} attacks for {self.attack_damage} damage")
---
+++ 2_oop/2.2_encapsulation/main.py
@@ -0,0 +1,18 @@
+from enemy import *
+
+ene1=Enemy("Zombie")
+# print(f"{ene1.type_of_enemy} has {ene1.health_point} health points and can do attack of {ene1.attack_damage}")
+
+ene1.talk()
+ene1.walk_forward()
+ene1.attack()
+print("enemy1 is type of ", ene1.get_type_of_enemy())
+print()
+
+ene2=Enemy("Danab",20,2)
+# print(f"{ene2.type_of_enemy} has {ene2.health_point} health points and can do attack of {ene2.attack_damage}")
+
+ene2.talk()
+ene2.walk_forward()
+ene2.attack()
+print("enemy2 is type of "+ ene2.get_type_of_enemy())
