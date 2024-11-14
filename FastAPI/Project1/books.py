cc8e6acf4bafe53a3b15824e61707045ed5f43c3
rupjyoti921 <rupjyoti921@gmail.com>
2024-11-14 20:12:38 +0530

    learning get request

--- FastAPI/Project1/books.py
+++ FastAPI/Project1/books.py
@@ -3,12 +3,27 @@ from fastapi import FastAPI
 app= FastAPI()
 
 BOOKS=[
-    {"title1":"title_one", "author1":"author_one", "category1":"Science"}, 
-    {"title2":"title_two", "author2":"author_two", "category2":"Biology"},  
-    {"title3":"title_three", "author3":"author_three", "category3":"Computer"}  
+    {"title":"title_one", "author":"author_one", "category":"Science"}, 
+    {"title":"title_two", "author":"author_two", "category":"Biology"},  
+    {"title":"title_three", "author":"author_three", "category":"Computer"}  
 
 ]
 
+# normal get request for all books
 @app.get("/books")
 async def all_books():
     return BOOKS
+
+# get request for one book
+@app.get("/books/{onebook}")
+async def one_book(onebook:str):
+    for book in BOOKS:
+        if book.get("title").casefold() == onebook.casefold():
+            return book
+
+# get request using query
+@app.get("/books/")
+async def read_book_by_query(category:str):
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            return book
